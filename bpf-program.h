be18d8347e0dca16d8ec4affc1650a6834a172db
Alexander Clouter <aclouter@telemetry.com>
2014-07-25 23:29:11 +0100

    fix program

--- bpf-program.h
+++ bpf-program.h
@@ -1,27 +1,23 @@
 struct bpf_insn bpf_insns[] = {
-	/* load C[0] into R[0] */
+	/* R[0] = C[0] */
 	BPF_STMT(BPF_LD+BPF_ABS, 0),
 	BPF_STMT(BPF_ST+BPF_REC, 0),
 
-	/* load C[1] into R[1] */
+	/* R[1] = C[1] */
 	BPF_STMT(BPF_LD+BPF_ABS, 1),
 	BPF_STMT(BPF_ST+BPF_REC, 1),
 
-	/* fetch from G */
+	/* find in G */
 	BPF_STMT(BPF_MISC+BPF_LDR, 0),
 	
-	/* load R[2] into A, incremental and put back */
-	BPF_STMT(BPF_LD+BPF_REC, 2),
-	BPF_STMT(BPF_ALU+BPF_ADD+BPF_K, 1),
-	BPF_STMT(BPF_ST+BPF_REC, 2),
-
-	/* load R[3] into A, incremental by R[1] and put back */
+	/* R[3] += R[1] */
+	BPF_STMT(BPF_LD+BPF_REC, 1),
+	BPF_STMT(BPF_MISC+BPF_TAX, 0),
 	BPF_STMT(BPF_LD+BPF_REC, 3),
-	BPF_STMT(BPF_LDX+BPF_REC, 1),
 	BPF_STMT(BPF_ALU+BPF_ADD+BPF_X, 0),
 	BPF_STMT(BPF_ST+BPF_REC, 3),
 
-	/* R[] into G */
+	/* store in G */
 	BPF_STMT(BPF_RET+BPF_K, 4),
 };
 
