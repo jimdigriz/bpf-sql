f6a735ec2c7b4257be9906fbfaaecb8ea61f2242
Alexander Clouter <aclouter@telemetry.com>
2014-07-26 12:37:56 +0100

    add an example of by-hour with a filter

---
+++ programs/filter-by-hour.h
@@ -0,0 +1,64 @@
+/* SELECT HOUR,COUNT(TV2NSPID),SUM(TV2NSPID) WHERE 36000 < TV2NSPID < 36100 OR 36650 < TV2NSPID < 36700 */
+
+struct bpf_insn bpf_insns[] = {
+	/* 36000 < C[1] < 36100 || 36650 < C[1] < 36700 */
+	BPF_STMT(BPF_LD+BPF_ABS, 1),
+	BPF_JUMP(BPF_JMP+BPF_JGT+BPF_K, 36000, 0, 1),
+	BPF_JUMP(BPF_JMP+BPF_JGE+BPF_K, 36100, 0, 3),
+	BPF_JUMP(BPF_JMP+BPF_JGT+BPF_K, 36650, 0, 1),
+	BPF_JUMP(BPF_JMP+BPF_JGE+BPF_K, 36700, 0, 1),
+
+	BPF_STMT(BPF_RET+BPF_K, 0),
+
+	/* R[0] = C[0] - C[0] % 3600 */
+	BPF_STMT(BPF_LD+BPF_ABS, 0),
+	BPF_STMT(BPF_ALU+BPF_DIV+BPF_K, 3600),
+	BPF_STMT(BPF_ALU+BPF_MUL+BPF_K, 3600),
+	BPF_STMT(BPF_MISC+BPF_TAX, 0),
+	BPF_STMT(BPF_LD+BPF_ABS, 0),
+	BPF_STMT(BPF_ALU+BPF_SUB+BPF_X, 0),
+	BPF_STMT(BPF_MISC+BPF_TAX, 0),
+	BPF_STMT(BPF_LD+BPF_ABS, 0),
+	BPF_STMT(BPF_ALU+BPF_SUB+BPF_X, 0),
+	BPF_STMT(BPF_ST+BPF_REC, 0),
+
+	/* find in G */
+	BPF_STMT(BPF_MISC+BPF_LDR, 0),
+	
+	/* R[1]++ */
+	BPF_STMT(BPF_LD+BPF_REC, 1),
+	BPF_STMT(BPF_ALU+BPF_ADD+BPF_K, 1),
+	BPF_STMT(BPF_ST+BPF_REC, 1),
+
+	/* R[2] += C[1] */
+	BPF_STMT(BPF_LD+BPF_ABS, 1),
+	BPF_STMT(BPF_MISC+BPF_TAX, 0),
+	BPF_STMT(BPF_LD+BPF_REC, 2),
+	BPF_STMT(BPF_ALU+BPF_ADD+BPF_X, 0),
+	BPF_STMT(BPF_ST+BPF_REC, 2),
+
+	/* store in G */
+	BPF_STMT(BPF_RET+BPF_K, 3),
+};
+
+struct bpf_program bpf_prog = {
+	.bf_len		= sizeof(bpf_insns)/sizeof(struct bpf_insn),
+	.bf_insns	= bpf_insns,
+};
+
+#define HACK_CSIZE 2 /* bpf_sql.ncols */
+#define HACK_KSIZE 1 /* bpf_sql.nkeys */
+#define HACK_RSIZE 2 /* bpf_sql.width */
+
+bpf_sql_t bpf_sql = {
+	.ncols	= 2,
+	.col	= {
+			"day16265.tim.bin",
+			"day16265.tv2nspid.bin"
+	},
+
+	.nkeys	= 1,
+	.width	= 2,
+
+	.prog	= &bpf_prog,
+};
